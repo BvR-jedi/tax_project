17a2aec383940c88384cc5ae6929a702e7f367cc
Borys <Borys>
2026-04-28 22:15:06 -0700

    reinit

--- tax_project/wsgi.py
+++ tax_project/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for tax_project project.
+WSGI config for tax_project2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tax_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tax_project2.settings')
 
 application = get_wsgi_application()
