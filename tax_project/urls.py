00a62d1d15ab4f7f3ce983a07822e8859e04aab7
Borys <Borys>
2026-05-08 21:57:46 -0700

    Add appointment call-to-action to index page and update URL configuration

--- tax_project/urls.py
+++ tax_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('tax.urls')),
     path('', include('appointments.urls')),
-    path('tax/', include('tax.urls')),
 ]
